75aee6e0fae7e0c720c5cd1d0b0f73a8bd7d110f
acsps-svg <andrewcesar.p@gmail.com>
2026-02-03 22:15:08 -0300

    Mudança para um teste simples

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -3,16 +3,9 @@ from playwright.sync_api import Page, expect
 
 
 def test_example(page: Page) -> None:
-    page.goto("http://127.0.0.1:5000/")
+    page.goto("http://127.0.0.1:5000")
     page.get_by_role("textbox", name="Nome da nova pessoa").click()
-    page.get_by_role("textbox", name="Nome da nova pessoa").fill("Teste 123")
+    page.get_by_role("textbox", name="Nome da nova pessoa").fill("Andrew")
     page.get_by_role("button", name="Criar").click()
-    page.locator("#goalInput").dblclick()
-    page.locator("#goalInput").dblclick()
-    page.locator("#goalInput").press("ControlOrMeta+a")
-    page.locator("#goalInput").fill("3000")
-    page.locator("#goalInput").press("Tab")
-    page.locator("#cupInput").fill("300")
-    page.locator("#cupInput").press("Enter")
 
-    expect(page.get_by_text("300", exact=True)).to_be_visible()
+    expect(page.get_by_role("button", name="Beber Copo")).to_be_visible()
